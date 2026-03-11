4fb5728526bfcabea904784e4b2733a79ed48daa
debadritahalder-coder <debadritahalder@gmail.com>
2026-03-11 09:40:14 +0530

    Add HelloworldUC2 class with greeting functionality

---
+++ HelloworldUC2.java
@@ -0,0 +1,8 @@
+public class HelloworldUC2 {
+    public static void main(String[] args) {
+
+        String name = args[0];   // taking name from command line
+
+        System.out.println("Hello " + name + "! Welcome.");
+    }
+}
